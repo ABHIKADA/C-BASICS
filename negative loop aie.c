3e1d3a4b1c3519eaa57962fc9d980caea0852b0b
ABHINAY KADA <96563720+ABHIKADA@users.noreply.github.com>
2022-01-06 11:35:44 +0530

    Add files via upload

---
+++ negative loop aie.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int a,d;
+	printf("ee");
+	scanf("%d%d",&a,&d);
+	while(d>a)
+	{
+		printf("%d",a);
+		a++;
+	}
+}
---
+++ while loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int i=12;
+	while(i<18)//18<18
+	{
+		printf ("%d",i);//12 13 14 15 16 17 
+		i++;
+	}
+	
+}
